c35c15fda58bee9c1c7a6f155284a3bd8b225159
Gift_Kawisara <kawisara_th@cmu.ac.th>
2020-08-11 13:19:33 +0700

    nav wel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -77,19 +77,19 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
+            <!-- @if (Route::has('login')) -->
                 <div class="top-right links">
-                    @auth
+                    <!-- @auth -->
                         <a href="{{ url('/home') }}">Home</a>
-                    @else
+                    <!-- @else -->
                         <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
+                        <!-- @if (Route::has('register')) -->
                             <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+                        <!-- @endif -->
+                    <!-- @endauth -->
                 </div>
-            @endif
+            <!-- @endif -->
 
             <div class="content">
                 <div class="title m-b-md">
